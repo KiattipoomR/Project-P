7da73073a3578c648adb411c281d122efc2668bf
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-03-12 19:53:58 +0700

    Player movement rework

--- Assets/Scripts/Player/MovementManager.cs
+++ Assets/Scripts/Player/MovementManager.cs
@@ -1,57 +1,65 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MovementManager : MonoBehaviour
+public class MovementManager : MonoBehaviour, PlayerControls.IPlayerActions
 {
     // Components
-    Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
 
     // Player attributes
-    float playerMoveSpeed = 5f;
-    float playerMoveSpeedLimit = 0.7f;
-    float inputHorizontal;
-    float inputVertical;
+    [SerializeField] private float movementSpeed = 5f;
+    private PlayerControls playerControls;
+    private Vector2 playerMovement;
 
     // Animations and states
-    Animator animator;
-    string currentAnimationState;
+    private string currentAnimationState;
 
-    const string PLAYER_IDLE_FRONT = "Player_Idle_Front";
-    const string PLAYER_IDLE_BACK = "Player_Idle_Back";
-    const string PLAYER_IDLE_RIGHT = "Player_Idle_Right";
-    const string PLAYER_IDLE_LEFT = "Player_Idle_Left";
-    const string PLAYER_WALK_UP = "Player_Walk_Up";
-    const string PLAYER_WALK_DOWN = "Player_Walk_Down";
-    const string PLAYER_WALK_RIGHT = "Player_Walk_Right";
-    const string PLAYER_WALK_LEFT = "Player_Walk_Left";
+    private const string PLAYER_IDLE_FRONT = "Player_Idle_Front";
+    private const string PLAYER_IDLE_BACK = "Player_Idle_Back";
+    private const string PLAYER_IDLE_RIGHT = "Player_Idle_Right";
+    private const string PLAYER_IDLE_LEFT = "Player_Idle_Left";
+    private const string PLAYER_WALK_UP = "Player_Walk_Up";
+    private const string PLAYER_WALK_DOWN = "Player_Walk_Down";
+    private const string PLAYER_WALK_RIGHT = "Player_Walk_Right";
+    private const string PLAYER_WALK_LEFT = "Player_Walk_Left";
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        animator = gameObject.GetComponent<Animator>();
+        playerControls = new PlayerControls();
+        playerControls.Player.SetCallbacks(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        // Handle inputs
-        inputHorizontal = Input.GetAxisRaw("Horizontal");
-        inputVertical = Input.GetAxisRaw("Vertical");
+        playerControls.Enable();
+    }
 
-        if (inputHorizontal > 0)
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        playerMovement = context.ReadValue<Vector2>();
+
+        if (playerMovement.x > 0)
         {
             ChangeAnimationState(PLAYER_WALK_RIGHT);
         }
-        else if (inputHorizontal < 0)
+        else if (playerMovement.x < 0)
         {
             ChangeAnimationState(PLAYER_WALK_LEFT);
         }
-        else if (inputVertical > 0)
+        else if (playerMovement.y > 0)
         {
             ChangeAnimationState(PLAYER_WALK_UP);
         }
-        else if (inputVertical < 0)
+        else if (playerMovement.y < 0)
         {
             ChangeAnimationState(PLAYER_WALK_DOWN);
         }
@@ -83,20 +91,7 @@ public class MovementManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Resolve movements
-        if (inputHorizontal != 0 || inputVertical != 0)
-        {
-            if (inputHorizontal != 0 && inputVertical != 0)
-            {
-                inputHorizontal *= playerMoveSpeedLimit;
-                inputVertical *= playerMoveSpeedLimit;
-            }
-            rb.velocity = new Vector2(inputHorizontal * playerMoveSpeed, inputVertical * playerMoveSpeed);
-        }
-        else
-        {
-            rb.velocity = new Vector2(0f, 0f);
-        }
+        rb.velocity = playerMovement * movementSpeed;
     }
 
     // Change player animation states
