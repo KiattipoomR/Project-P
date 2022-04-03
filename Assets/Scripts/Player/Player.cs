3df3e90a8fa3886c29d3a11c7b79f88e69df268a
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-03 21:24:03 +0700

    Proto player movement with no animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class Player : SingletonMonobehaviour<Player>
 {
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb;
 
+    [Header("Attributes")]
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Vector2 playerMovement;
+
+    private void OnMovement(InputValue ctx)
+    {
+        playerMovement = ctx.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = playerMovement * movementSpeed;
+    }
 }
