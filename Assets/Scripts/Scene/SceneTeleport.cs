331326383d7fa90d81272c2e9dec3c86d0d684f9
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-05 22:54:17 +0700

    Test change between scenes

---
+++ Assets/Scripts/Scene/SceneTeleport.cs
@@ -0,0 +1,28 @@
+using Manager;
+using UnityEngine;
+
+namespace Scene
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class SceneTeleport : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private string destinationSceneName;
+        [SerializeField] private Vector3 destinationSpawnPoint = new Vector3();
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Player.Player player = other.GetComponent<Player.Player>();
+            if (player == null) return;
+
+            float xPos = Mathf.Approximately(destinationSpawnPoint.x, 0f)
+                ? player.transform.position.x
+                : destinationSpawnPoint.x;
+            float yPos = Mathf.Approximately(destinationSpawnPoint.y, 0f)
+                ? player.transform.position.y
+                : destinationSpawnPoint.y;
+            
+            SceneControllerManager.Instance.ChangeScene(destinationSceneName, new Vector3(xPos, yPos, 0f));
+        }
+    }
+}
