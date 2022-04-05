fc2295411a4bc7a2225027131b596e77e6bfda4b
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-05 16:09:07 +0700

    Relocate and rename files

--- Assets/Scripts/Item/ItemData.cs
+++ Assets/Scripts/Item/ItemData.cs
@@ -4,9 +4,10 @@ namespace Item
 {
     [System.Serializable]
     [CreateAssetMenu(menuName = "Scriptable Object/Item", fileName = "Item_ItemData")]
-    public class ItemDetail : ScriptableObject
+    public class ItemData : ScriptableObject
     {
         public int itemID;
+        public string itemName;
         public string itemDescription;
         public Sprite itemIcon;
     }
