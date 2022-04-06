d99c344a7d11189d9507a0af46a5d99beabdcc22
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-06 10:03:23 +0700

    Injects item data for the item entity

--- Assets/Scripts/Entity/ItemEntity.cs
+++ Assets/Scripts/Entity/ItemEntity.cs
@@ -1,13 +1,21 @@
+using Item;
 using UnityEngine;
 
 namespace Entity
 {
     public class ItemEntity : MonoBehaviour
     {
+        [Header("Components")]
+        [SerializeField] private ItemData item;
+
         private SpriteRenderer _renderer;
+        
         private void Awake()
         {
             _renderer = GetComponentInChildren<SpriteRenderer>();
+
+            if (!item) return;
+            _renderer.sprite = item.itemIcon;
         }
     }
 }
