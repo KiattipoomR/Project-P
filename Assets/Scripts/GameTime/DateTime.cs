0d447e7443057d550d2fe2e63e5a2811244e7841
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-06 14:40:03 +0700

    Date and time system

---
+++ Assets/Scripts/GameTime/DateTime.cs
@@ -0,0 +1,115 @@
+namespace GameTime
+{
+    [System.Serializable]
+    public struct DateTime
+    {
+        private int _year;
+        private Season _season;
+
+        private Day _day;
+        private int _date;
+
+        private int _hour;
+        private int _minute;
+
+        private int _totalDaysPassed;
+
+        public int Year => _year;
+        public Season Season => _season;
+        public Day Day => _day;
+        public int Date => _date;
+        public int Hour => _hour;
+        public int Minute => _minute;
+        public int TotalDaysPassed => _totalDaysPassed;
+
+        public DateTime(int year, int season, int date, int hour, int minute)
+        {
+            _year = year;
+            _season = (Season)(season - 1);
+
+            _day = (Day)((date - 1) % 7);
+            _date = date;
+
+            _hour = hour;
+            _minute = minute;
+
+            _totalDaysPassed = date + ((int)_season * 28) + ((_year - 1) * 28 * 4);
+        }
+
+        public void AdvanceMinute(int deltaMinutes)
+        {
+            _minute = (_minute + deltaMinutes) % 60;
+            if (_minute == 0) AdvanceHour();
+
+            if (!(_hour == 2 && _minute == 10)) return;
+
+            AdvanceDay();
+            _hour = 7;
+            _minute = 0;
+        }
+
+        private void AdvanceHour()
+        {
+            _hour = (_hour + 1) % 24;
+        }
+
+        private void AdvanceDay()
+        {
+            _day = (Day)(((int)_day + 1) % 7);
+            _date = (_date + 1) % 29;
+            _totalDaysPassed++;
+
+            if (_date != 0) return;
+
+            AdvanceSeason();
+            _date = 1;
+        }
+
+        private void AdvanceSeason()
+        {
+            _season = (Season)(((int)_season + 1) % 4);
+            if (_season == 0) AdvanceYear();
+        }
+
+        private void AdvanceYear()
+        {
+            _year++;
+        }
+
+        public override string ToString()
+        {
+            return $"{TimeToString()}, {DateToString()}";
+        }
+
+        private string DateToString()
+        {
+            return $"{Day} {Date}, {Season} Year {Year:D2}";
+        }
+
+        private string TimeToString()
+        {
+            return $"{Hour:D2}:{Minute:D2}";
+        }
+    }
+
+    [System.Serializable]
+    public enum Day
+    {
+        Mon,
+        Tue,
+        Wed,
+        Thu,
+        Fri,
+        Sat,
+        Sun,
+    }
+
+    [System.Serializable]
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter,
+    }
+}
---
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,76 @@
+using DateTime = GameTime.DateTime;
+using Misc;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Manager
+{
+    public class TimeManager : SingletonMonobehaviour<TimeManager>
+    {
+        [Header("Date & Time Settings")]
+        [Range(1, 99)]
+        [SerializeField] private int year;
+        [Range(1, 4)]
+        [SerializeField] private int season;
+        [Range(1, 28)]
+        [SerializeField] private int date;
+        [Range(0, 24)]
+        [SerializeField] private int hour;
+        [Range(0, 6)]
+        [SerializeField] private int minute;
+
+        [Header("Tick Settings")]
+        [SerializeField] private int tickMinutesIncrease = 10;
+        [SerializeField] private float timeBetweenTicks = 1f;
+
+        public UnityAction<DateTime> OnDateTimeChanged;
+
+        private float _currentTimeBetweenTicks;
+        private bool _isPaused;
+        private DateTime _currentTime;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _currentTime = new DateTime(year, season, date, hour, minute * 10);
+        }
+
+        private void Start()
+        {
+            OnDateTimeChanged?.Invoke(_currentTime);
+        }
+
+        private void OnEnable()
+        {
+            PauseManager.Instance.OnPauseTriggered += ControlTime;
+        }
+
+        private void OnDisable()
+        {
+            PauseManager.Instance.OnPauseTriggered -= ControlTime;
+        }
+
+        private void Update()
+        {
+            if (_isPaused) return;
+
+            _currentTimeBetweenTicks += Time.deltaTime;
+            if (_currentTimeBetweenTicks < timeBetweenTicks) return;
+            
+            _currentTimeBetweenTicks = 0;
+            TimeAdvance();
+        }
+
+        private void TimeAdvance()
+        {
+            Debug.Log(_currentTime);
+            _currentTime.AdvanceMinute(tickMinutesIncrease);
+            OnDateTimeChanged?.Invoke(_currentTime);
+        }
+
+        private void ControlTime(bool isPaused)
+        {
+            _isPaused = isPaused;
+        }
+    }
+}
