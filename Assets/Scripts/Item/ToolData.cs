7a54f95f87ab18d453938f48903d900a7abef6bd
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-04-06 21:57:55 +0700

    Add more tool properties and add test tool asset

--- Assets/Scripts/Item/ToolData.cs
+++ Assets/Scripts/Item/ToolData.cs
@@ -6,8 +6,26 @@ namespace Item
     [CreateAssetMenu(menuName = "Scriptable Object/Tool", fileName = "Item_Tool_ToolData")]
     public class ToolData : ItemData
     {
+        [Header("Tool Attributes")]
+        [SerializeField] private ToolType toolType;
+        [SerializeField] private int staminaCost;
+        
+        
         public override int MaxStack => 1;
         public override int BuyPrice => 0;
         public override int SellPrice => 0;
+        
+        public ToolType ToolType => toolType;
+        public int StaminaCost => staminaCost;
+    }
+
+    [System.Serializable]
+    public enum ToolType
+    {
+        Pickaxe,
+        Axe,
+        Reaper,
+        Hoe,
+        WateringCan,
     }
 }
