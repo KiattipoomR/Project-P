fdd422fd043dc8558697ad93fccdab2f0e254feb
thanabodee thamasroch <bankthamasaroch@gmail.com>
2022-05-30 19:41:02 +0700

    fix Shop and feat alert message

--- Assets/Scripts/UI/ShopItemUI.cs
+++ Assets/Scripts/UI/ShopItemUI.cs
@@ -12,20 +12,34 @@ namespace UI
     public class ShopItemUI : MonoBehaviour
     {
         [SerializeField] private CurrencyManager currencyManager;
-        
 
+        public GameObject popupPanel;
         public SeedData seedData;
 
         public Image artworkImage;
 
         public TextMeshProUGUI priceText;
+
+        public TextMeshProUGUI popupMessage;
+
+        private bool status;
         
         // Start is called before the first frame update
 
         public void OnClick()
         {
-            print("Click");
-            currencyManager.subtraceRune(seedData.BuyPrice);
+            status = currencyManager.subtraceRune(seedData.BuyPrice);
+
+            if (status != true)
+            {
+                popupMessage.text = "Not have enough money";
+                popupPanel.SetActive(true);
+            }
+        }
+
+        public void ClosePopUp()
+        {
+            popupPanel.SetActive(false);
         }
   
         void Start()
